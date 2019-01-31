64869c4551aede4d7ce3b262de968840ccc0580e
semal259 <semalpatel2596@gmail.com>
2019-01-31 01:31:38 -0800

    remove contact form and replace with simple mailto:

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,8 +14,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "semalpatel2596@gmail.com"; // This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n";
+$header = "From: noreply@chnolo.gy\n";
 $header .= "Reply-To: $email";
+mail($to, $subject, $body, $header);
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
